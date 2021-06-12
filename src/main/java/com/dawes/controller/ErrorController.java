bf1147851fa310c2f75bb8c04fb726b472037f77
Sergio Acebal <sergio98.am30@gmail.com>
2021-06-12 18:14:41 +0200

    Página de error personalizada

--- src/main/java/com/dawes/controller/ErrorController.java
+++ src/main/java/com/dawes/controller/ErrorController.java
@@ -1,13 +1,17 @@
 package com.dawes.controller;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /*
  * CONTROLADOR PARA MAPEAR LAS VISTAS DE LOS ERRORES
  */
 
 @Controller
+@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
 public class ErrorController {
 
 	//Error 404
@@ -20,5 +24,11 @@ public class ErrorController {
 	@GetMapping("/403")
 	public String error403() {
 		return "error/403";
+	}
+	
+	//Errores
+	@RequestMapping("/error")
+	public String error() {
+		return "error";
 	}
 }
